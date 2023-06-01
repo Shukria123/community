f50a24ccb282c3d1ecb7deca9cf9d90f172c16bf
shukria123 <shukria.ahmadi1999@gmail.com>
2023-06-01 18:02:32 +0430

    make change in switch show to hide and hide to show passwords and some changes in colors done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/updatepass.dart';
+import 'package:flutter_application_1/update_pass.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 85, 62, 124)),
-        useMaterial3: true,
-      ),
-      home: updatepass(),
+      home: UpdatePass(),
     );
   }
 }
--- lib/update_pass.dart
+++ lib/update_pass.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
 
-class updatepass extends StatelessWidget {
+class UpdatePass extends StatefulWidget {
+  @override
+  State<UpdatePass> createState() => _UpdatePassState();
+}
+
+class _UpdatePassState extends State<UpdatePass> {
   bool _obscureText = true;
+  bool _newPassword = true;
+  bool _confPassword = true;
+
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return SafeArea(
       // ignore: prefer_const_constructors
       child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 222, 227, 230),
+        appBar: AppBar(
+          title: Text("Academy"),
+          leading: Container(
+            child: Image.asset('assets/images/logo.png'),
+          ),
+          elevation: 10,
+          backgroundColor: Color.fromARGB(255, 224, 227, 231),
+        ),
+
         // ignore: prefer_const_constructors, prefer_const_literals_to_create_immutables
         body: Container(
           // ignore: prefer_const_constructors
@@ -29,9 +47,12 @@ class updatepass extends StatelessWidget {
         Text(
           "Update Password",
           style: TextStyle(
-            fontSize: 30,
+            fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
+        ),
+        SizedBox(
+          height: 9,
         ),
       ],
     );
@@ -41,13 +62,16 @@ class updatepass extends StatelessWidget {
     return Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
       Text(
         "Current Password",
-        style: TextStyle(fontSize: 16),
+        style: TextStyle(fontSize: 15),
+      ),
+      SizedBox(
+        height: 3,
       ),
       TextField(
         decoration: InputDecoration(
             hintText: "Current Password",
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
+              borderRadius: BorderRadius.circular(16),
               borderSide: BorderSide.none,
             ),
             fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
@@ -55,7 +79,9 @@ class updatepass extends StatelessWidget {
             prefixIcon: Icon(Icons.key),
             suffixIcon: GestureDetector(
               onTap: () {
-                _obscureText = !_obscureText;
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
               },
               child:
                   Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
@@ -63,17 +89,20 @@ class updatepass extends StatelessWidget {
         obscureText: _obscureText,
       ),
       SizedBox(
-        height: 10,
+        height: 4,
       ),
       Text(
         "New Password",
-        style: TextStyle(fontSize: 16),
+        style: TextStyle(fontSize: 15),
+      ),
+      SizedBox(
+        height: 3,
       ),
       TextField(
         decoration: InputDecoration(
             hintText: "New Password",
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
+              borderRadius: BorderRadius.circular(16),
               borderSide: BorderSide.none,
             ),
             fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
@@ -81,25 +110,30 @@ class updatepass extends StatelessWidget {
             prefixIcon: Icon(Icons.key),
             suffixIcon: GestureDetector(
               onTap: () {
-                _obscureText = !_obscureText;
+                setState(() {
+                  _newPassword = !_newPassword;
+                });
               },
               child:
-                  Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                  Icon(_newPassword ? Icons.visibility : Icons.visibility_off),
             )),
-        obscureText: _obscureText,
+        obscureText: _newPassword,
       ),
       SizedBox(
-        height: 10,
+        height: 4,
       ),
       Text(
         "Confirm Password",
-        style: TextStyle(fontSize: 16),
+        style: TextStyle(fontSize: 15),
+      ),
+      SizedBox(
+        height: 3,
       ),
       TextField(
         decoration: InputDecoration(
             hintText: "Confirm Password",
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(18),
+              borderRadius: BorderRadius.circular(16),
               borderSide: BorderSide.none,
             ),
             fillColor: Theme.of(context).primaryColor.withOpacity(0.1),
@@ -107,15 +141,17 @@ class updatepass extends StatelessWidget {
             prefixIcon: Icon(Icons.key),
             suffixIcon: GestureDetector(
               onTap: () {
-                _obscureText = !_obscureText;
+                setState(() {
+                  _confPassword = !_confPassword;
+                });
               },
               child:
-                  Icon(_obscureText ? Icons.visibility : Icons.visibility_off),
+                  Icon(_confPassword ? Icons.visibility : Icons.visibility_off),
             )),
-        obscureText: _obscureText,
+        obscureText: _confPassword,
       ),
       SizedBox(
-        height: 10,
+        height: 8,
       ),
       ElevatedButton(
         onPressed: () {},
@@ -126,7 +162,7 @@ class updatepass extends StatelessWidget {
         style: ElevatedButton.styleFrom(
           shape: StadiumBorder(),
           padding: EdgeInsets.symmetric(vertical: 16),
-          backgroundColor: Color.fromARGB(255, 237, 62, 62),
+          backgroundColor: Color.fromARGB(255, 205, 81, 81),
         ),
       ),
     ]);
